4ed030a55b2b892d7a9d99279533033fbdfe5bcb
[name] <sergio.gamboa11@gmail.com>
2018-10-15 19:25:13 -0600

    Made client send many put requests for testing

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -5,7 +5,7 @@ import socket, sys, re, os
 
 sys.path.append("../lib")       # for params
 import params
-
+from threading import Thread
 from framedSockThread import FramedStreamSock
 
 
@@ -31,45 +31,56 @@ except:
     print("Can't parse server:port from '%s'" % server)
     sys.exit(1)
 
-s = None
-for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
-    af, socktype, proto, canonname, sa = res
-    try:
-        print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
-        s = socket.socket(af, socktype, proto)
-    except socket.error as msg:
-        print(" error: %s" % msg)
-        s = None
-        continue
-    try:
-        print(" attempting to connect to %s" % repr(sa))
-        s.connect(sa)
-    except socket.error as msg:
-        print(" error: %s" % msg)
-        s.close()
-        s = None
-        continue
-    break
 
-if s is None:
-    print('could not open socket')
-    sys.exit(1)
+class ClientThread(Thread):
+    def __init__(self, serverHost, serverPort, debug):
+        Thread.__init__(self, daemon=False)
+        self.serverHost, self.serverPort, self.debug = serverHost, serverPort, debug
+        self.start()
+    def run(self):
+       s = None
+       for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+           af, socktype, proto, canonname, sa = res
+           try:
+               print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+               s = socket.socket(af, socktype, proto)
+           except socket.error as msg:
+               print(" error: %s" % msg)
+               s = None
+               continue
+           try:
+               print(" attempting to connect to %s" % repr(sa))
+               s.connect(sa)
+           except socket.error as msg:
+               print(" error: %s" % msg)
+               s.close()
+               s = None
+               continue
+           break
 
-# If the file that was input by the user exists, we open and read its contents
-# and encode them to send them to server. If the file exists but it its size is 0,
-# we don't send it to the server
-if os.path.exists(file) and file!=" " and os.path.getsize(file) == 0:
-    print("Error: File size is 0")
-    exit()
-elif os.path.exists(file) and file!=" ":
-    f = open(file, "r")
-    outMessage = file + "::" + f.read();
-    outMessage = outMessage.replace("\n", "\\n")
-else:
-    print("Error: File not found")
-    exit()
-fs = FramedStreamSock(s, debug=debug)
-outMessageB = bytes(outMessage, encoding='utf-8')
-print("sending " + outMessage)
-fs.sendmsg(outMessageB)
-print("received:", fs.receivemsg())
+       if s is None:
+           print('could not open socket')
+           sys.exit(1)
+
+       # If the file that was input by the user exists, we open and read its contents
+       # and encode them to send them to server. If the file exists but it its size is 0,
+       # we don't send it to the server
+       if os.path.exists(file) and file != " " and os.path.getsize(file) == 0:
+           print("Error: File size is 0")
+           exit()
+       elif os.path.exists(file) and file != " ":
+           f = open(file, "r")
+           outMessage = file + "::" + f.read();
+           outMessage = outMessage.replace("\n", "\\n")
+       else:
+           print("Error: File not found")
+           exit()
+       fs = FramedStreamSock(s, debug=debug)
+       outMessageB = bytes(outMessage, encoding='utf-8')
+       print("sending " + outMessage)
+       fs.sendmsg(outMessageB)
+       print("received:", fs.receivemsg())
+
+
+for i in range(100):
+    ClientThread(serverHost, serverPort, debug)
